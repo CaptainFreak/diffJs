434f81e0370a4c95129c85b3863045e8934e36ae
CaptainFreak <patelshoeb4@gmail.com>
2018-03-18 20:32:56 +0530

    08:32PM on March 18, 2018

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 mkdir temp
 mkdir dependency
 mkdir results
+chmod +x ./javascript_files_extractor.py
+chmod +x ./javascript_files_link_extractor.sh
 cd tools
 git clone https://github.com/jobertabma/relative-url-extractor.git
 sudo apt-get install git 
